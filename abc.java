ce0746d6a9fda058feccbd36929bf69dbfe96175
kakar786 <52959938+kakar786@users.noreply.github.com>
2020-09-24 09:21:21 +0430

    Update abc.java

--- abc.java
+++ abc.java
@@ -2,9 +2,9 @@ int a = 10;
 
 int b = 13;
 
-Add = a + b;
-Sub = a - b;
-Mult = a * b;
-Divi = a / b;
+int Add = a + b;
+int Sub = a - b;
+int Mult = a * b;
+int Divi = a / b;
 
 system.out.println("10 + 13 = " + Add + "10 - 13 = " + Sub + "10 * 13 = " + Mult + "10 / 13 = " + Divi);
