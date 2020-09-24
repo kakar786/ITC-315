3f2f431c98786ad63479dad7690917af510f8ebb
kakar786 <52959938+kakar786@users.noreply.github.com>
2020-09-24 09:16:59 +0430

    Create abc.java
    It will do basic arithmetic operations

---
+++ abc.java
@@ -0,0 +1,10 @@
+int a = 10;
+
+int b = 13;
+
+Add = a + b;
+Sub = a - b;
+Mult = a * b;
+Divi = a / b;
+
+system.out.println("10 + 13 = " + Add + "10 - 13 = " + Sub + "10 * 13 = " + Mult + "10 / 13 = " + Divi;
