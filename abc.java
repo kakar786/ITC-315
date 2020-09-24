84318933634b87fbb623bcbc7f74467390f5140c
kakar786 <52959938+kakar786@users.noreply.github.com>
2020-09-24 09:33:04 +0430

    Revert "Update abc.java"

--- abc.java
+++ abc.java
@@ -2,8 +2,9 @@ int a = 10;
 
 int b = 13;
 
+int Add = a + b;
 int Sub = a - b;
 int Mult = a * b;
 int Divi = a / b;
 
-system.out.println("10 - 13 = " + Sub + "10 * 13 = " + Mult + "10 / 13 = " + Divi);
+system.out.println("10 + 13 = " + Add + "10 - 13 = " + Sub + "10 * 13 = " + Mult + "10 / 13 = " + Divi);
